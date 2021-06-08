2bd0bb95cbc0f20819757474bfccb57cf5aac6c4
sam <samheldin.1999@gmail.com>
2021-06-08 14:29:07 +0300

    Starting App.js Stuff

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,48 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/Drawer";
 import {auth, users} from "./firebase_functions";
 
+
+class Client {
+    constructor(document, document_data) {
+        this.doc = document;
+        this.name = document_data.name;
+        this.cart = document_data.cart;
+    }
+
+    async addItem(item_id) {
+        if (this.cart == null) {
+            console.log('Cannot add item, something wrong with data');
+            return;
+        }
+
+        // Add new item to the cart with quantity 1 or
+        // increase by 1 if already exist
+        if (!this.cart.hasOwnProperty(item_id))
+            this.cart[item_id] = 0;
+        this.cart[item_id]++;
+        // Might need to update visually as well!
+        await this.doc.update({cart: this.cart}).then(() => {
+            console.log('update successfully!');
+        }).catch((error) => {
+            console.log('error with update! ' + error);
+        });
+    };
+
+    getCartDataInListForm() {
+
+        return (
+            <div>
+                <p>   User Cart</p>
+            <ul>
+                {Object.keys(this.cart).map((item)=>{
+                    return <li key={item}>{item}: {this.cart[item]}</li>
+                })}
+            </ul>
+            </div>
+        );
+    };
+}
+
 export function App(props) {
     const [user, setUser] = useState(null);
     const [drawer_open, setDrawerOpen] = useState(false);
@@ -20,9 +62,10 @@ export function App(props) {
         return auth.onAuthStateChanged(u => {
             if (u) {
                 setUser(u);
-                users.doc(u.email).get().then((doc)=>{
-                    setClient(doc.data());
-                });
+                users.doc(u.email).get().then((document) => {
+                    setClient(new Client(users.doc(u.email), document.data()));
+                    console.log('client was loaded successfully from database');
+                }).catch(error => console.log('Something went wrong when getting client, ' + error));
             } else {
                 props.history.push("/");
             }
@@ -39,16 +82,7 @@ export function App(props) {
                 alert(error.message);
             });
     };
-    const getClientCart = (client) => {
-        if(!client)
-            return;
-        if(client.cart == null)
-            client.cart = [];
-        client.cart.push(25);
-        for(const item in client.cart)
-            console.log(item);
-        console.log('completed');
-    }
+
     if (!user) {
         return <div/>;
     }
@@ -74,8 +108,8 @@ export function App(props) {
                         My App
                     </Typography>
                     <Typography color="inherit" style={{marginRight: 30}}>
-                        Hi! {client ? client.name : ""}
-                        {getClientCart(client)}
+                        Hi! {client && client.name ? client.name : ""}
+                        {}
                     </Typography>
                     <Button color="inherit" onClick={handleSignOut}>
                         Sign out
@@ -85,6 +119,14 @@ export function App(props) {
             <Drawer open={drawer_open} onClose={handleCloseDrawer}>
                 I'm a drawer
             </Drawer>
+            {/*THIS BUTTON IS FOR TESTING, CAN BE DELETED OR MODIFIED.*/}
+            <Button color="inherit" onClick={() => {
+                client.addItem(25).then(r => console.log(r ? r : ' '));
+            }}>
+                TEST ADD ITEM NUMBER 25 TO THE CURRENT USER
+            </Button>
+            {/* Check if null*/}
+            {client ? client.getCartDataInListForm() : ''}
         </div>
     );
 }
